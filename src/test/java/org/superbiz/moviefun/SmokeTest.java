7fa46b1580a5aeb39632ebfb664172f7281670ae
Common Training user <CT0t1868@TechMahindra.com>
2019-05-29 13:53:07 +0530

    buildPack

--- src/test/java/org/superbiz/moviefun/SmokeTest.java
+++ src/test/java/org/superbiz/moviefun/SmokeTest.java
@@ -31,7 +31,8 @@ public class SmokeTest {
     }
 
     private String url(String path) {
-        String baseUrl = "http://localhost:8080/";
+       String baseUrl = "http://localhost:8080/";
+
         String envUrl = System.getenv("MOVIE_FUN_URL");
 
         if (envUrl != null && !envUrl.isEmpty()) {
